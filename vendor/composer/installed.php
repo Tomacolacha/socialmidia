b904c4e416dbde0543d2aa513ec67d4720a55606
Macolacha <tomasgermacolacha@gmail.com>
2025-09-26 10:21:11 +0200

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'socialmidia/socialmidia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'de74d40c342c0ab90690626795fa54690fb71276',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'socialmidia/socialmidia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'de74d40c342c0ab90690626795fa54690fb71276',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
